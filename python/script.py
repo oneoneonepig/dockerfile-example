8c2f4a38e942bd8de7b7786af30538416571187d
Jeffrey Chu <peihuachu1112@gmail.com>
2020-07-21 11:18:27 +0800

    Change all output message to Hello, world!
    Signed-off-by: Jeffrey Chu <peihuachu1112@gmail.com>

--- python/script.py
+++ python/script.py
@@ -1,9 +1,1 @@
-import camelcase
-c = camelcase.CamelCase()
-try:
-  print("Start typing: ")
-  while True:
-    txt = input("")
-    print(c.hump(txt))
-except KeyboardInterrupt:
-  print("Exiting")
+print("Hello, world!")
