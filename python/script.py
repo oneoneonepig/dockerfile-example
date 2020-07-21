ed25446a3ede4a0346141cfc809213a293e0a5d7
Jeffrey Chu <peihuachu1112@gmail.com>
2020-07-21 12:14:20 +0800

    Change from stdout to http server
    Signed-off-by: Jeffrey Chu <peihuachu1112@gmail.com>

--- python/script.py
+++ python/script.py
@@ -1,1 +1,9 @@
-print("Hello, world!")
+import http.server
+import socketserver
+
+PORT = 80
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+  print("serving at port", PORT)
+  httpd.serve_forever()
