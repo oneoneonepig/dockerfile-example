b98d5a124f30964fb06f4c98ace2866b857a1dc9
Jeffrey Chu <peihuachu1112@gmail.com>
2020-07-28 10:01:42 +0000

    Change from output message to web server
    Signed-off-by: Jeffrey Chu <peihuachu1112@gmail.com>

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Println("Hello, world!")
+	// Configuration
+	gin.SetMode(gin.ReleaseMode)
+
+	// Routing
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "Hello, world!")
+	})
+
+	// Running
+	if err := r.Run(":80"); err != nil {
+		panic(err)
+	}
 }
